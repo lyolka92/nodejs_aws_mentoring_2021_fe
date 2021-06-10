a0f7ea5a54820567e9619114e2beeaf518a81f53
Olga Serova <lyolka92@gmail.com>
2021-06-10 18:00:31 +0300

    add integration with product-service API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
