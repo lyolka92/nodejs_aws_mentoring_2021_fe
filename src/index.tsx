a2536e5070e07262547106d10e761c14414f5ec9
Olga Serova <lyolka92@gmail.com>
2021-08-26 15:50:35 +0300

    implement authentication for file import and its error handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.css";
+import App from "components/App/App";
+import { store } from "store/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import axios from 'axios';
+import axios from "axios";
+
+const SUPPORTED_ERROR_CODES = [400, 401, 403];
 
 axios.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  function(error) {
-    if (error?.response?.status === 400) {
-      alert(error.response.data?.data);
+  function (error) {
+    if (SUPPORTED_ERROR_CODES.includes(error?.response?.status)) {
+      alert(error.response.data?.data || error.response.data?.message);
     }
 
     return Promise.reject(error?.response ?? error);
@@ -24,11 +26,11 @@ axios.interceptors.response.use(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline/>
-      <App/>
+      <CssBaseline />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
