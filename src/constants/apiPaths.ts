e1daab3ac2bc8811bf647424790d5156a6c2dcfe
Olga Serova <lyolka92@gmail.com>
2021-09-20 15:33:58 +0300

    implement task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
-
 const API_PATHS = {
-  product: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1m0hisd8r5.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev',
+  product: "https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://1m0hisd8r5.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://78x9p2bww6.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://lyolka92-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
